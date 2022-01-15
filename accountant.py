a97010bf13a842568855fc8938d8e9b98af8a2b6
Sławomir Borczyk <slawek_python@outlook.com>
2022-01-15 10:38:51 +0100

    Dodatnie sys.argv, ale wyrzuca blad

--- accountant.py
+++ accountant.py
@@ -7,11 +7,11 @@ przeglad = []
 historia = []
 
 while True:
-    czynn_gosp = input()
+    czynn_gosp = input('Podaj czynn. gosp: ')
     if czynn_gosp == "zakup":
-        nazwa_prod = input()
-        cena_prod = int(input())
-        ilosc = int(input())
+        nazwa_prod = input('Podaj nazwę produktu: ')
+        cena_prod = int(input('Podaj cenę: '))
+        ilosc = int(input('Podaj ilość: '))
         if ilosc < 0:
             print("Nieprawidłowa ilość.")
             continue
@@ -19,7 +19,7 @@ while True:
             print('Brak środków na zakup!')
             continue
         saldo -= cena_prod * ilosc
-        historia.append((czynn_gosp, cena_prod, ilosc))
+        historia.append((czynn_gosp, nazwa_prod, cena_prod, ilosc))
         if nazwa_prod in magazyn:
             magazyn[nazwa_prod] += ilosc
             print('Produkty w magazynie {}'.format(magazyn))
@@ -28,26 +28,26 @@ while True:
             print('Produkty w magazynie {}'.format(magazyn))
 
     elif czynn_gosp == "saldo":
-        wartosc = int(input())
+        wartosc = int(input('Podaj wartość: '))
         if saldo + wartosc < 0:
             print('Debet na koncie')
         else:
             saldo = saldo + wartosc
-            komentarz = input()
+            komentarz = input('Podaj komentarz: ')
             saldo_operacje[komentarz] = wartosc
             historia.append((czynn_gosp, wartosc, komentarz))
             print(saldo_operacje, saldo)
 
     elif czynn_gosp == "sprzedaz":
-        nazwa_prod = input()
+        nazwa_prod = input('Podaj nazwę produktu: ')
         if nazwa_prod not in magazyn:
             print('Brak produktu w magazynie {}'.format(magazyn))
             continue
-        cena_jedno_produktu_sprzed = int(input())
+        cena_jedno_produktu_sprzed = int(input('Podaj cenę jdn: '))
         if cena_jedno_produktu_sprzed < 0:
             print('Błąd, cena mniejsza od zera\n')
             continue
-        ilosc = int(input())
+        ilosc = int(input('Podaj ilość: '))
         if ilosc < 0:
             print('Niesłaściwa ilość \n')
             continue
@@ -87,12 +87,10 @@ elif czynn_gosp == "zakup":
     ilosc = int(sys.argv[4])
     if ilosc < 0:
         print("Nieprawidłowa ilość.")
-        continue
     if saldo - cena_prod * ilosc < 0:
         print('Brak środków na zakup!')
-        continue
     saldo -= cena_prod * ilosc
-    historia.append((czynn_gosp, cena_prod, ilosc))
+    historia.append((czynn_gosp, nazwa_prod, cena_prod, ilosc))
     if nazwa_prod in magazyn:
         magazyn[nazwa_prod] += ilosc
         print('Produkty w magazynie {}'.format(magazyn))
@@ -104,24 +102,20 @@ elif czynn_gosp == "sprzedaz":
     nazwa_prod = sys.argv[2]
     if nazwa_prod not in magazyn:
         print('Brak produktu w magazynie {}'.format(magazyn))
-        continue
     cena_jedno_produktu_sprzed = int(sys.argv[3])
     if cena_jedno_produktu_sprzed < 0:
         print('Błąd, cena mniejsza od zera\n')
-        continue
     ilosc = int(sys.argv[4])
     if ilosc < 0:
         print('Niesłaściwa ilość \n')
-        continue
     if magazyn[nazwa_prod] < ilosc:
         print('Brak wystarczającej ilości produktów na magazynie')
-        continue
     saldo += cena_jedno_produktu_sprzed * ilosc
     magazyn[nazwa_prod] -= ilosc
     historia.append((czynn_gosp, nazwa_prod, cena_jedno_produktu_sprzed, ilosc))
     print('Saldo konta {}\n'.format(saldo))
 
-elif czynn_gosp == "stop":
+"""elif czynn_gosp == "stop":
     print('Koniec')
     print(historia)
-    break
+    break"""
